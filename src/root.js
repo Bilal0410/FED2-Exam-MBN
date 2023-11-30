c40e4db331474d33aa93699046f88ea4f40d0e08
Bilal0410 <bilaln0410@gmail.com>
2023-11-30 12:08:01 +0100

    vite, tailwind installation and router set up

---
+++ src/root.js
@@ -0,0 +1,30 @@
+import { Router, Route, RootRoute } from "@tanstack/react-router";
+import Root from "./App";
+import LoginForm from "./components/login";
+
+const rootRoute = new RootRoute({
+  component: Root,
+});
+
+// NOTE: @see https://tanstack.com/router/v1/docs/guide/routes
+
+const indexRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: LoginForm,
+});
+
+const loginRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/login",
+  component: LoginForm,
+});
+
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  loginRoute,
+]);
+
+export const router = new Router({ routeTree });
+
+export default router;
