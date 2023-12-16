e9e432b9b1c9ca65d6e7d92fb381a3225140bcb6
Bilal0410 <bilaln0410@gmail.com>
2023-12-17 00:06:14 +0100

    making the listing show based on latest data

--- src/components/listings/index.jsx
+++ src/components/listings/index.jsx
@@ -1,47 +1,50 @@
 import { useEffect, useState } from "react";
 
 function AllListings() {
-  // State to store the auction listings
   const [auctionListings, setAuctionListings] = useState([]);
-  // State to store the filtered listings based on the search query
   const [filteredAuctionListings, setFilteredAuctionListings] = useState([]);
-  // State to hold the search query
   const [searchQuery, setSearchQuery] = useState("");
-
-  // State to trigger data refresh
   const [refreshData, setRefreshData] = useState(false);
+  const [bidAmounts, setBidAmounts] = useState({});
 
-  // Function to fetch data from the API
-  const fetchData = () => {
-    fetch("https://api.noroff.dev/api/v1/auction/listings")
-      .then((response) => response.json())
-      .then((data) => {
-        // Sort the listings in descending order based on creation date
-        const sortedListings = data.sort(
-          (a, b) => new Date(b.created) - new Date(a.created)
-        );
-        setAuctionListings(sortedListings);
-        setFilteredAuctionListings(sortedListings); // Initially set filtered listings to all listings
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  };
-
-  // Fetch data from the API when the component mounts or when refreshData changes
   useEffect(() => {
     const fetchDataAndResetRefresh = async () => {
-      await fetchData();
+      try {
+        // Retrieve the access token from local storage
+        const accessToken = localStorage.getItem("access_token");
+
+        const response = await fetch(
+          "https://api.noroff.dev/api/v1/auction/listings?sort=created&sortOrder=desc",
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          const sortedListings = data.sort(
+            (a, b) => new Date(b.created) - new Date(a.created)
+          );
+          setAuctionListings(sortedListings);
+          setFilteredAuctionListings(sortedListings);
+        } else {
+          console.error("Error fetching data:", response.status);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
       setRefreshData(false);
     };
 
     fetchDataAndResetRefresh();
-  }, [refreshData]); // Include refreshData in the dependency array
+  }, [refreshData]);
 
-  // Function to handle search input change
   const handleSearchChange = (e) => {
     const query = e.target.value.toLowerCase();
     setSearchQuery(query);
 
-    // Filter listings based on the search query
     const filteredListings = auctionListings.filter(
       (listing) =>
         listing.title.toLowerCase().includes(query) ||
@@ -51,9 +54,50 @@ function AllListings() {
     setFilteredAuctionListings(filteredListings);
   };
 
+  const handleBidSubmit = async (listingId) => {
+    try {
+      const response = await fetch(
+        `https://api.noroff.dev/api/v1/auction/listings/${listingId}/bids`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          },
+          body: JSON.stringify({
+            amount: Number(bidAmounts[listingId]) || 0,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        console.log("Bid placed successfully!");
+
+        // Retrieve current user credits from local storage
+        const currentUserCredits = JSON.parse(
+          localStorage.getItem("user_credits") || "0"
+        );
+
+        // Update user credits and set it back to local storage
+        const updatedUserCredits =
+          currentUserCredits - (Number(bidAmounts[listingId]) || 0);
+        localStorage.setItem(
+          "user_credits",
+          JSON.stringify(updatedUserCredits)
+        );
+
+        setRefreshData(true);
+      } else {
+        const errorData = await response.json();
+        console.error("Error submitting bid:", response.status, errorData);
+      }
+    } catch (error) {
+      console.error("Error submitting bid:", error);
+    }
+  };
+
   return (
     <div className="container mx-auto p-4">
-      {/* Search Input */}
       <input
         type="text"
         placeholder="Search through listings..."
@@ -77,6 +121,35 @@ function AllListings() {
             </p>
             <p className="text-gray-600">Bids: {listing._count.bids}</p>
             <img className="mt-4" src={listing.media[0]} alt={listing.title} />
+
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleBidSubmit(listing.id);
+              }}
+            >
+              <label>
+                Bid Amount:
+                <input
+                  type="number"
+                  value={bidAmounts[listing.id] || 0}
+                  onChange={(e) =>
+                    setBidAmounts((prevBidAmounts) => ({
+                      ...prevBidAmounts,
+                      [listing.id]: e.target.value,
+                    }))
+                  }
+                  required
+                  className="bg-white text-black ml-2 border rounded-md p-1"
+                />
+              </label>
+              <button
+                type="submit"
+                className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2"
+              >
+                Place Bid
+              </button>
+            </form>
           </div>
         ))}
       </div>
